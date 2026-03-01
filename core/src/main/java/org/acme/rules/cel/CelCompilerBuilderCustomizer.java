658ce473d55d7481988468c51eff90a7437aba65
Hu Rui <hr101191@gmail.com>
2026-03-02 00:42:03 +0800

    added codegen stubs

--- core/src/main/java/org/acme/rules/cel/CelCompilerBuilderCustomizer.java
+++ core/src/main/java/org/acme/rules/cel/CelCompilerBuilderCustomizer.java
@@ -1,10 +1,14 @@
 package org.acme.rules.cel;
 
 import dev.cel.compiler.CelCompilerBuilder;
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.VertxGen;
 
+@VertxGen
 @FunctionalInterface
 public interface CelCompilerBuilderCustomizer {
 
+    @GenIgnore
     void customize(CelCompilerBuilder celCompilerBuilder);
 
 }
--- core/src/main/java/org/acme/rules/cel/CelRuntimeBuilderCustomizer.java
+++ core/src/main/java/org/acme/rules/cel/CelRuntimeBuilderCustomizer.java
@@ -1,10 +1,14 @@
 package org.acme.rules.cel;
 
 import dev.cel.runtime.CelRuntimeBuilder;
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.VertxGen;
 
+@VertxGen
 @FunctionalInterface
 public interface CelRuntimeBuilderCustomizer {
 
+    @GenIgnore
     void customize(CelRuntimeBuilder celRuntimeBuilder);
     
 }
